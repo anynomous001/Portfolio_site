6b1e4e4b2af69173a3f05421a596e7547391c2fb
PRITAM CHAKROBORTY <92992609+anynomous001@users.noreply.github.com>
2023-09-04 21:33:31 +0530

    Add files via upload

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -3,15 +3,18 @@ import Navbar from '../components/Navbar'
 import Hero2 from '../components/Hero2'
 import Footer from '../components/Footer'
 import contact from '../assets/contact.jpg'
+import Form from '../components/Form'
 
 
 const Contact = () => {
     return (
-        <div> <Navbar />
+        <div>
+            <Navbar />
             <Hero2
                 img={contact}
                 headline={'Contact Me.'}
                 text={'Email me at - Chakrobortypritam1@gmail.com'} />
+            <Form />
             <Footer />
         </div>
     )
